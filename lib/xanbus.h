aff020a87dbf46258e6b7f718cb98c759101e85a
Rob Savoye <rob@welcomehome.org>
2011-12-11 14:00:05 -0700

    add data structure for xanbus commands

--- lib/xanbus.h
+++ lib/xanbus.h
@@ -33,11 +33,51 @@
 class XanbusUI : public Serial
 {
 public:
+
+
+    
     typedef enum {USERMENU, SETUPMENU} menu_e;
     enum inverter_e {OFF, SRCH, ON, CHG};
     enum invertsetup_e {FLOAT, SELL, SLT, LBX};
     enum battery_e {NICAD, LEADACID, GEL};
     enum genauto_e {UNINITIALIZED, RUNNING, IDLE};
+
+    typedef enum { XACCESS, ACCESS_QUERY, BACKLIGHT, CONT, CONT_QUERY,
+                   CUSTOM11_QUERY, CUSTOM12_QUERY, CUSTOM11ERASE,
+                   CUSTOM12ERASE, CUSTOM21, CUSTOM22, CUSTOM21_QUERY,
+                   CUSTOM22_QUERY, CUSTOM21ERASE, CUSTOM22ERASE,
+                   DCCOMPLEV_QUERY, DCDC_QUERY, DERATELIMIT_QUERY,
+                   ERR_QUERY, FAULTACTIVE_QUERY, FAULTGND_QUERY,
+                   FAULTLAST_QUERY, FAULTLOG_QUERY, FREQ_QUERY,
+                   FWHW_QUERY, IDN_QUERY, IIN_QUERY, INV_QUERY,
+                   IOUT_QUERY, KNOCK, KWHLIFE_QUERY, KWHLIFESYS_QUERY,
+                   KWHTODAY_QUERY, KWHTODAYSYS_QUERY, XKWHTODAY_QUERY,
+                   MEASENGY_QUERY, MEASENGYSYS_QUERY, MEASICMM_QUERY,
+                   MEASIN_QUERY, MEASOUT_QUERY, MEASRAW_QUERY, MEASTEMP_QUERY,
+                   MEMFREE_QUERY, MODELID_QUERY, MODELSTR_QUERY, MONPARAM_QUERY,
+                   MONALL_QUERY, MONAUXPARAM_QUERY, MONENS_QUERY,
+                   MONENSPARAM_QUERY, MONEXT_QUERY, MONEXTPARAM_QUERY,
+                   MONFB_QUERY, MONFBPARAM_QUERY, MONGND_QUERY,
+                   MONGNDPARAM_QUERY, MONGRID_QUERY, MONGRIDPARAM_QUERY,
+                   MONTEMP_QUERY, MONTEMPPARAM_QUERY, MONOVPARAM_QUERY,
+                   MONUVPARAM_QUERY, MONVIN_QUERY, MPPT_QUERY,
+                   MPPTDBG_QUERY, MPPTLEV_QUERY, MPPTLEVM_QUERY,
+                   MPPTOFFSET_QUERY, MPPTSTAT_QUERY, NVMEMSPACE_QUERY,
+                   OFFSET_QUERY, OLDERLIM_QUERY, PDCMPPT_QUERY, PIN_QUERY,
+                   POUT_QUERY, POUTSYS_QUERY, PROD_QUERY, POWLIMIT_QUERY,
+                   POWSEQ_QUERY, POWSEQCLR, QUEUED_QUERY, RECTIME_QUERY,
+                   RELAY_QUERY, RIPCOMP_QUERY, ROM_QUERY, SCALE_QUERY,
+                   SCID_QUERY, SCROLL, STANDBY, STANDBY_QUERY, TEMPLIMIT_QUERY,
+                   TEMPLIMITSTEP_QUERY, TIME_QUERY, VDCREF_QUERY, VIN_QUERY,
+                   XBADX_QUERY
+    } xanbus_command_e;
+
+
+    typedef struct {
+        xanbus_command_e type;
+        std::string      command;
+        bool             query;
+    } cmd_data_t;
     
     XanbusUI(void);
     ~XanbusUI(void);
@@ -155,3 +195,4 @@ extern "C" {
 // mode: C++
 // indent-tabs-mode: nil
 // End:
+
