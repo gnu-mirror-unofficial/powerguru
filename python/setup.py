d17c55033465daf1eb50ac1d08e79fecf1fba178
Rob Savoye <rob@senecass.com>
2019-01-05 13:51:48 -0700

    Python3 support files

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PowerGuru',
+    version='0.1',
+    packages=[],
+    url='https://git.savannah.gnu.org/git/powerguru.git',
+    license='GPLv3',
+    author='Rob Savoye',
+    author_email='rob@senecass.com',
+    description='.',
+)
