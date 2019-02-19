1990fc173cfccc0227f252c41c5a8dd85394e609
Rob Savoye <rob@senecass.com>
2019-02-19 12:22:17 -0700

    Refactored from different project, now works

--- python/postgresql.py
+++ python/postgresql.py
@@ -17,59 +17,93 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 
-import pdb
+import epdb
 import glob
 import psycopg2
 import subprocess
 import logging
+from options import CmdOptions
 
 
-class postgresql(object):
+class Postgresql(object):
     """A class to work with a postgresql database"""
     def __init__(self):
-        self.dbname = ""
+        self.options = CmdOptions()
+        self.connect()
         self.result = ""
 
-    def connect(self, dbname):
+    def connect(self):
         """Connect to a postgresql server"""
-        logging.debug("plotcalls.connect(" + dbname + ")")
-        self.dbname = "";
-        connect = " dbname=" + dbname
-        
-        try:
-            self.dbshell = psycopg2.connect(connect)
-            if self.dbshell.closed == 0:
-                self.dbshell.autocommit = True
-                logging.info("Opened connection to %r %r" % (dbname, self.dbshell))
-                
-                self.dbcursor = self.dbshell.cursor()
-                if self.dbcursor.closed == 0:
-                    logging.info("Opened cursor in %r %r" % (dbname, self.dbcursor))
 
-        except Exception as e:
-            print("Couldn't connect to database: %r" % e)
+        # Supported parameters for connect are: 
+        # *database*: the database name (only as keyword argument)
+        # *user*: user name used to authenticate
+        # *password*: password used to authenticate
+        # *host*: database host address (defaults to UNIX socket if not provided)
+        # *port*: connection port number (defaults to 5432 if not provided)
+        dbserver = self.options.get('dbserver')
+        database = self.options.get('database')
+        connect = ""
+        if dbserver is not "localhost":
+            connect += "host='" + dbserver + "'"
+        connect += " dbname='" + database + "'"
 
-    def query(self, query, nores=""):
+        logging.debug("postgresql.connect(%r)" % connect)
+        self.dbshell = psycopg2.connect(connect)
+        if self.dbshell.closed == 0:
+            self.dbshell.autocommit = True
+            logging.info("Opened connection to %r %r" % (database, self.dbshell))
+
+            self.dbcursor = self.dbshell.cursor()
+            if self.dbcursor.closed == 0:
+                logging.info("Opened cursor in %r %r" % (database, self.dbcursor))
+
+    def query(self, query):
         """Query a postgresql database"""
-        logging.debug("pgdb.query(" + query + ")")
+
+        logging.debug("postgresql.query(" + query + ")")
+        #epdb.set_trace()
+        if self.dbshell.closed != 0:
+            logging.error("Database %r is not connected!" % self.options.get('database'))
+            return self.result
+
+        self.dbcursor.execute(query)
         try:
-            self.dbcursor.execute(query)
             self.result = self.dbcursor.fetchall()
-        except:
-            self.result = list()
-        #logging.debug("FIXME: query(%r)" % len(self.result))
-        nores = self.result
+        except psycopg2.ProgrammingError as e:
+            if e.pgcode != None:
+                logging.error("Query failed to fetch! %r" % e.pgcode)
+
+        #logging.debug("Returned %r rows from query: %r" % (self.dbcursor.rowcount, self.dbcursor.statusmessage))
+        #if self.dbcursor.rowcount is None:
+        parsed = query.split(' ')
+        table = ""
+        if parsed[0] == "INSERT":
+            table = parsed[2]
+        if self.dbcursor.statusmessage == "INSERT 0 1":
+            logging.debug("Inserted into %r" % (table))
+            self.result = self.dbcursor.statusmessage
+        elif self.dbcursor.statusmessage == "INSERT 0 0":
+            logging.debug("Already exists in database %r" % (table))
         return self.result
 
+    def isConnected(self):
+        if self.dbshell.closed == 0:
+            return True
+        else:
+            return False
+
     def dump(self):
-        print("Dumping data from pgdb class")
-        self.list_functions()
+        print("Dumping data from postgresql class")
+        print("\tDB server: %r" % self.options.get('dbserver'))
+        print("\tDatabase: %r" % self.options.get('database'))
+        #self.list_functions()
         if self.dbshell.closed == 0:
             status = "Open"
         else:
             status = "Closed"
-        print("Connection: " + status)
-        print("DSN: " + self.dbshell.dsn)
-        print("AutoCommit: " + str(self.dbshell.autocommit))
+        print("\tConnection: " + status)
+        print("\tDSN: " + self.dbshell.dsn)
+        print("\tAutoCommit: " + str(self.dbshell.autocommit))
         for i in self.dbshell.notices:
-            print("History: " + i.replace('\n', ''))
+            print("\tHistory: " + i.replace('\n', ''))
