6f12901d474141ab5162bdba94ffbfa26656db9a
Rob Savoye <rob@senecass.com>
2019-02-19 12:25:07 -0700

    Use new options and postgresql classes

---
+++ python/gpio433.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# 
+#   Copyright (C) 2019 Free Software Foundation, Inc.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+
+# API documentation at: https://pyownet.readthedocs.io/en/latest/
+
+import sys
+import epdb
+import logging
+import time
+#import psycopg2
+import sensor
+from datetime import datetime
+import platform
+import RPi.GPIO as GPIO
+from options import CmdOptions
+
+RECEIVED_SIGNAL = [[], []]  #[[time of reading], [signal reading]]
+MAX_DURATION = 20
+RECEIVE_PIN = 27
+
+def gpio433_handler(sensors):
+    logging.debug("Start gpio433...")
+
+    options = CmdOptions()
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RECEIVE_PIN, GPIO.IN)
+    cumulative_time = 0
+    beginning_time = datetime.now()
+    print('**Started recording**')
+    while cumulative_time < MAX_DURATION:
+        time_delta = datetime.now() - beginning_time
+        RECEIVED_SIGNAL[0].append(time_delta)
+        RECEIVED_SIGNAL[1].append(GPIO.input(RECEIVE_PIN))
+        #print("%r\n" % RECEIVED_SIGNAL[1])
+        cumulative_time = time_delta.seconds
+    print('**Ended recording**')
+    print("%r samples recorded" % len(RECEIVED_SIGNAL[0]))
+    GPIO.cleanup()
+
+    print('**Processing results**')
+    for i in range(len(RECEIVED_SIGNAL[0])):
+        RECEIVED_SIGNAL[0][i] = RECEIVED_SIGNAL[0][i].seconds + RECEIVED_SIGNAL[0][i].microseconds/1000000.0
+
+    print('**Done processing results**')
+
