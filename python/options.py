9e8883d000c3948c343e092a32544bfb754d0542
Rob Savoye <rob@senecass.com>
2019-02-21 20:51:39 -0700

    Use pydoc style comments for GPLD block

--- python/options.py
+++ python/options.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-# 
-#   Copyright (C) 2019 Free Software Foundation, Inc.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+"""
+   Copyright (C) 2019 Free Software Foundation, Inc.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
 
 import epdb
 import logging
@@ -28,12 +28,21 @@ from proc.core import Process
 class CmdOptions(object):
     """This class handles command line options between threads in
     a thread-safe way by using the /proc filesystem read-only."""
+
+    # Setup default command line options
+    options = dict()
+    helper = """\t--help(-h)   Help
+        \t--owserver(-o)    OW server [host[:port]], default '%s'
+        \t--dbserver(-p)    Database server [host]:port]], default '%s'
+        \t--database(-d)    Database name, default '%s'
+        \t--interval(-i)    Set the time interval for device polling, default '%s'
+        \t--verbose(-v)     Enable verbosity
+        \t--scale(-s)       Set the temperature scale, 'F' or 'C', default '%s'
+        """
+
     def __init__(self):
         """Read the command line option from /proc/PID/cmdline"""
         proc = Process.from_path('/proc/self')
-        self.options = dict()
-        # Setup default command line options
-        self.options = dict()
         self.options['interval'] = 300  # time interval to delay when polling for data
         self.options['owserver'] = "localhost"  # hostname of the owserver
         self.options['dbserver'] = "localhost"  # hostname of the database
@@ -41,23 +50,24 @@ class CmdOptions(object):
         self.options['database'] = "powerguru"  # The database name
         self.options['verbosity'] = logging.ERROR
         self.processArgs(proc.cmdline[2:])
+        self.options['exec'] = os.path.basename(proc.cmdline[1])
 
-        # menu for --help
-    def usage(self):
+    def usage(self, more=""):
         """Display command line option usage"""
+        epdb.set_trace()
         #print(argv[0] + ": self.options: ")
-        print("""\t--help(-h)   Help
-        \t--owserver(-o)    OW server [host[:port]], default '%s'
-        \t--dbserver(-p)    Database server [host]:port]], default '%s'
-        \t--database(-d)    Database name, default '%s'
-        \t--interval(-i)    Set the time interval for device polling, default '%s'
-        \t--verbose(-v)     Enable verbosity
-        \t--scale(-s)       Set the temperature scale, 'F' or 'C', default '%s'
-        """ % (self.options['owserver'],
-               self.options['dbserver'],
-               self.options['database'],
-               self.options['interval'],
-               self.options['scale']))
+        #    query += """\tStarttime: %s
+        #    \tEndtime: %s""" % (self.options['starttime'], self.options['endtime'])
+        foo = self.helper % (self.options['owserver'],
+        self.options['dbserver'],
+        self.options['database'],
+        self.options['interval'],
+        self.options['scale'])
+        if len(more) > 0:
+            print(foo.rstrip() + more)
+        else:
+            print(foo)
+        #print(self.help.rstrip() + more)
         quit()
 
     # Check command line arguments
@@ -66,13 +76,11 @@ class CmdOptions(object):
         internal data."""
         logging.debug("options.processArgs: %r" % args)
 
-        #epdb.set_trace()
-        #if len(args) == 0:
-        #    self.usage()
-        
+        # this is the full set of supported options by all the
+        # utility programs, pgd.py, chart.py. mergedb.py.
         try:
-            (opts, val) = getopt.getopt(args, "h,o:,i:,p:,d:,s:,v,",
-                        ["help", "owserver", "dbserver", "database", "scale", "interval", "verbosity"])
+            (opts, val) = getopt.getopt(args, "h,o:,i:,p:,d:,s:,v,t:,e:",
+                        ["help", "owserver", "dbserver", "database", "scale", "interval", "verbosity" "starttime", "endtime"])
         except getopt.GetoptError as e:
             logging.error('%r' % e)
             self.usage()
@@ -88,6 +96,10 @@ class CmdOptions(object):
             #print("%r: %r = %r" % (i, opt, val))
             if opt == '--help' or opt == '-h':
                 self.usage()
+            elif opt == "--starttime" or opt == '-t':
+                self.options['endtime'] = val
+            elif opt == "--endtime" or opt == '-e':
+                self.options['starttime'] = val
             elif opt == "--owserver" or opt == '-o':
                 self.options['owserver'] = val
             elif opt == "--interval" or opt == '-i':
